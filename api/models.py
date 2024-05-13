982e4785925f72318ccbeb4a85608fb19df7afae
Sina <sinahte@gmail.com>
2024-05-13 15:29:48 +0330

    Fix

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class UserType(models.Model):
   """Model for storing different user types (customer, admin, etc.)"""
   name = models.CharField(max_length=50)
-
+  
   def __str__(self):
     return self.name
 
